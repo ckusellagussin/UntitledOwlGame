4f095322d32b2150b31d0d92a6031c4e5951cd2a
Casey <Kusella-Gussin>
2019-10-17 02:11:04 +0100

    (Very Basic) Movement Mechanics 1.0
    This will never represent the final game!

---
+++ Untitled Owl Project/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotationSpeed = 1;
+    public Transform targetArea, Owl;
+    public float smoothSpeed = 0.125f;
+    float xMov, yMov;
+
+    void Start()
+    {
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        targetArea = this.transform.parent;
+        Owl = targetArea.transform.parent;
+
+    }
+
+
+    //Late update so objects being moved can be updated while in the camera without conflict between the object and camera
+
+    void LateUpdate()
+    {
+
+        cameraControl();
+
+    }
+
+    void cameraControl()
+    {
+
+        //The inputs for what happens when you move the mouse
+        xMov += Input.GetAxis("xMov") * rotationSpeed;
+        yMov -= Input.GetAxis("yMov") * rotationSpeed;
+
+
+        //Prevents camera from gliding away from player too much
+        yMov = Mathf.Clamp(yMov, -30, 50);
+
+        //Keep Camera focused on the target area
+        transform.LookAt(targetArea);      
+
+        //The player will rotate only if right mouse button is held
+        if(Input.GetMouseButtonDown(1))
+        {
+            targetArea.rotation = Quaternion.Euler(yMov, xMov, 0);
+            Owl.rotation = Quaternion.Euler(0, xMov, 0);
+        }
+
+       else
+
+        {
+            targetArea.rotation = Quaternion.Euler(yMov, xMov, 0);
+       
+
+        }
+
+
+    }
+
+
+}
---
+++ Untitled Owl Project/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+
+    public float Speed;
+    public float Clockwise = 5.00f;
+    public float aClockwise = -5.00f;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        OwlMovement();
+        transform.Translate(0, -0.04f ,0);
+
+    }
+
+    void OwlMovement()
+    {
+        //Movement for going forward and backward for the Owl
+
+        float Horizontal = Input.GetAxis("Horizontal") * -1.0f;
+        float Vertical = Input.GetAxis("Vertical") * -1.0f;
+       
+        Vector3 playerMovement = new Vector3(Horizontal, 0f, Vertical) * Speed * Time.deltaTime;
+
+        transform.Translate(playerMovement, Space.Self);
+
+
+        //How to elevate the owl
+
+        if(Input.GetKey("space"))
+        {
+            transform.Translate(0, 0.4f, 0);
+
+        }
+
+        //Rotation for the Owl
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, Time.deltaTime * Clockwise, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, Time.deltaTime * aClockwise, 0);
+
+        }
+
+
+        
+
+    }
+
+}
